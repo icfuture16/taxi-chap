456e11aeea704c864433654fced9dc2aab1cfa7e
27110 <ic.future16@gmail.com>
2024-06-07 02:01:44 +0000

    Initial commit

--- app.py
+++ app.py
@@ -6,27 +6,22 @@ from google.oauth2 import service_account
 
 app = Flask(__name__)
 
-# Chemin vers le fichier JSON des informations d'identification
-credentials = service_account.Credentials.from_service_account_file(
-    '/home/hlab/Desktop/taxi/credentials.json')
-
+# Initialiser les informations d'identification Google Cloud Vision
+credentials = service_account.Credentials.from_service_account_file('/home/hlab/Desktop/taxi/credentials.json')
 client = vision.ImageAnnotatorClient(credentials=credentials)
 
 @app.route('/webhook', methods=['POST'])
 def github_webhook():
     data = request.json
-    # Vérifiez que l'événement est un push
     if data['ref'] == 'refs/heads/main':
-        # Récupérer le fichier modifié
         for commit in data['commits']:
             for file in commit['modified']:
                 if 'img_prix/' in file:
-                    # Télécharger l'image depuis GitHub
                     image_url = f"https://raw.githubusercontent.com/{data['repository']['full_name']}/main/{file}"
                     response = requests.get(image_url)
                     image_content = response.content
 
-                    # Envoyer l'image à l'API Cloud Vision
+                    # Envoyer l'image à Google Cloud Vision pour analyse
                     image = vision.Image(content=image_content)
                     response = client.label_detection(image=image)
                     labels = response.label_annotations
